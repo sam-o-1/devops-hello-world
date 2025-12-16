0af7c16772939f584631a0e4a78929fc53cc9d22
sam-o-1 <sohamgosavi0508@gmail.com>
2025-12-16 10:42:32 +0000

    Upgrade to Microservices : Added Redis and Docker Compose

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from redis import Redis
+import os
+
 app = Flask(__name__)
+
+# Connect to Redis
+# Host is 'redis' because that will be the service name in docker-compose
+redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    return "SUCCESS: I am running inside a Docker container!"
+    # Increment the counter in the database
+    redis.incr('hits')
+    counter = str(redis.get('hits'), 'utf-8')
+    return "This site has been visited " + counter + " times!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
